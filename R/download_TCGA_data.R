592c8601be60bde68cd1bc7ba72a5467bc903114
Sergio Alonso <sergio.alonso.phd@gmail.com>
2025-03-19 14:58:33 +0100

    download_TCGA_data.R completed
    Manually uploaded. "Push" did not work

--- R/download_TCGA_data.R
+++ R/download_TCGA_data.R
@@ -3,45 +3,56 @@
 # install necessary libraries if necessary
 
 ins_packs <- installed.packages()[,"Package"]
-if(!"ggplot2" %in% ins_packs) install.packages("ggplot2")
 if(!"BiocManager" %in% ins_packs) install.packages("BiocManager")
 if(!"tidyr" %in% ins_packs) install.packages("tidyr")
 if(!"TCGAbiolinks" %in% ins_packs) BiocManager::install("TCGAbiolinks")
 
 # load required packages
 
-library(ggplot2)
 library(TCGAbiolinks)
 library(tidyr)
 library(data.table)
 
-# Store the data in environments by tumor type
 
-tumor.types <- c("LUAD","")
+# TCGA projects
 
-for(tumor in tumor.types) {
-  assign(tumor,new.env()) # create the environments
-}
+projects <- data.table(getGDCprojects())
+projects[grep("TCGA",id),tumor] -> tumor.types
 
-# download methylation data
+# download clinical, mutational, CNA, and methylation data
 
 for(tumor in tumor.types) {
   
-  with(get(tumor),{
+    project <- paste0("TCGA-",tumor)
     
-    methylation_query <- GDCquery(project = sprintf("TCGA-%s",tumor), 
+    methylation_query <- GDCquery(project = sprintf("TCGA-%s",tumor),
                                   data.category = "DNA Methylation",
                                   platform = "Illumina Human Methylation 450",
                                   data.type = "Methylation Beta Value")
     GDCdownload(methylation_query,files.per.chunk = 15,directory = "GDCdata/") 
-
-  })
-  
+    
+    
+    clinical_query <- GDCquery(project = project, 
+                               data.category = "Clinical",
+                               data.type = "Clinical Supplement",
+                               data.format = "BCR Biotab")
+    GDCdownload(clinical_query,files.per.chunk = 30,directory = "GDCdata/") 
+    
+    mutations_query <- GDCquery(project = project,
+                                data.category = "Simple Nucleotide Variation",
+                                data.type = "Masked Somatic Mutation")
+    GDCdownload(mutations_query,files.per.chunk = 15,directory = "GDCdata/") 
+    
+    cna_query <- GDCquery(project = project,
+                          data.category="Copy Number Variation",
+                          data.type="Allele-specific Copy Number Segment",
+                          workflow.type = "ASCAT3")
+    GDCdownload(cna_query,files.per.chunk = 15,directory = "GDCdata/")
+    
 }
 
-# NOTE from Sergio: this strategy will create very large objects eating up the memory 
-# perhaps a good option would be to download the data, save it to RDS objects 
-# in the large_files_folder, and then empty the environment
+
+
 
 
 
