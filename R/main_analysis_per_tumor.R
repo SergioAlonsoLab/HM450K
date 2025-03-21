734ce27bc9d36044db74f300457582ac4a3fb2ca
Sergio <sergio.alonso.phd@gmail.com>
2025-03-21 11:01:42 +0100

    added cancer genes from TruSight

--- R/main_analysis_per_tumor.R
+++ R/main_analysis_per_tumor.R
@@ -33,16 +33,22 @@ for(tumor in tumor.types) {
   
   # identify if there are clear groups of patiens according to the number of mutations (i.e. a mutator phenotype)
   
-  
-  
-  
-  
+  foo <- patients[!is.na(Nmut),list(bcr_patient_barcode,Nmut)]
+  foo[,Mut_Cluster := paste0("Cluster_",Mclust(G=1:4,modelNames="E",log2(Nmut+.1))$classification)]
+  foo[,N_in_Cluster := .N, by=Mut_Cluster]
+  plot1 <- ggplot(foo) + 
+    geom_histogram(aes(log10(Nmut+1),fill=Mut_Cluster),bins=round(nrow(foo)/10)) + 
+    ggtitle(paste("Mutations in",tumor))
+  plot1
+  patients <- merge(patients,foo[,list(bcr_patient_barcode,Mut_Cluster,N_in_Cluster)],all.x=T)
   
   # 50 most frequently mutated genes
   
-  mutations[Variant_Classification!="Silent",list(N=.N),by=Hugo_Symbol][order(N,decreasing=T)][1:50]
+  mutations <- merge(mutations,unique(patients[,list(bcr_patient_barcode,Mut_Cluster,N_in_Cluster)]),all.x=T)
   
-  
+  foo <- mutations[Variant_Classification!="Silent",list(N=.N),by=list(Hugo_Symbol,Mut_Cluster,N_in_Cluster)]
+  foo[,mut_freq:=N/N_in_Cluster]
+  foo[order(mut_freq,decreasing=T)]
   
   x <- dcast(mutations[Hugo_Symbol %in% genes.of.interest],bcr_patient_barcode ~ Hugo_Symbol,value.var = "HGVSp_Short",fun.aggregate = function(x) paste(unique(x) %>% sort,collapse="|"))
   x[x==""] <- "WT"
